f1140ae1f7860d11e3ac7c8b7024690c86c962b4
dizzybeaver <jbhersey1977@gmail.com>
2025-10-11 11:39:00 -0400

    Update variables_utils.py

--- src/config/variables_utils.py
+++ src/config/variables_utils.py
@@ -1,12 +1,21 @@
 """
-variables_utils.py - Ultra-Optimized Configuration System Utility Functions
-Version: 2025.10.04.03
+variables_utils.py
+Version: 2025.10.11.01
 Description: Complete utility functions for configuration estimation and validation
 
-DEPLOYMENT FIX: Changed from .variables to variables (absolute import)
-
 Copyright 2025 Joseph Hersey
-Licensed under the Apache License, Version 2.0
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 
 import time
