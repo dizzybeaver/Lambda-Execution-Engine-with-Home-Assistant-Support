52fd27e20fe1ca4c4aa5ee9f0c2c991eb02e6fc6
dizzybeaver <jbhersey1977@gmail.com>
2025-10-14 19:00:25 -0400

    Update __init__.py

--- src/security/__init__.py
+++ src/security/__init__.py
@@ -1,1 +1,70 @@
+"""
+security/__init__.py - Security module initialization
+Version: 2025.10.14.01
+Description: Exports all security operations for gateway integration
 
+Copyright 2025 Joseph Hersey
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from security_types import SecurityOperation, ValidationPattern
+from security_core import (
+    SecurityCore,
+    _execute_validate_request_implementation,
+    _execute_validate_token_implementation,
+    _execute_validate_string_implementation,
+    _execute_validate_email_implementation,
+    _execute_validate_url_implementation,
+    _execute_encrypt_implementation,
+    _execute_decrypt_implementation,
+    _execute_hash_implementation,
+    _execute_verify_hash_implementation,
+    _execute_sanitize_implementation,
+    _execute_generate_correlation_id_implementation,
+    _execute_encrypt_data_implementation,
+    _execute_decrypt_data_implementation,
+    _execute_hash_data_implementation,
+    _execute_sanitize_input_implementation,
+    validate_string_input,
+    validate_email_input,
+    validate_url_input,
+    get_security_stats
+)
+
+__all__ = [
+    'SecurityOperation',
+    'ValidationPattern',
+    'SecurityCore',
+    '_execute_validate_request_implementation',
+    '_execute_validate_token_implementation',
+    '_execute_validate_string_implementation',
+    '_execute_validate_email_implementation',
+    '_execute_validate_url_implementation',
+    '_execute_encrypt_implementation',
+    '_execute_decrypt_implementation',
+    '_execute_hash_implementation',
+    '_execute_verify_hash_implementation',
+    '_execute_sanitize_implementation',
+    '_execute_generate_correlation_id_implementation',
+    '_execute_encrypt_data_implementation',
+    '_execute_decrypt_data_implementation',
+    '_execute_hash_data_implementation',
+    '_execute_sanitize_input_implementation',
+    'validate_string_input',
+    'validate_email_input',
+    'validate_url_input',
+    'get_security_stats'
+]
+
+# EOF
