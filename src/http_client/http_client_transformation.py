d01c0817efbb09140e28e9d12fbe8d08e36c8734
Joseph Hersey <jbhersey1977@gmail.com>
2025-12-10 12:09:23 -0500

    Update http_client_transformation.py

--- src/http_client/http_client_transformation.py
+++ src/http_client/http_client_transformation.py
@@ -1,22 +1,8 @@
 """
-http_client_transformation.py - HTTP Response Transformation
-Version: 2025.10.14.01
-Description: Response transformation pipeline for HTTP client.
-             Internal module - accessed via http_client.py interface.
-
-Copyright 2025 Joseph Hersey
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+http_client/http_client_transformation.py
+Version: 2025-12-10_1
+Purpose: HTTP response transformation
+License: Apache 2.0
 """
 
 from typing import Dict, Any, Callable, Optional, List
@@ -122,16 +108,7 @@ class TransformationPipeline:
 
 
 def transform_http_response(response: Dict[str, Any], transformer: Callable) -> Dict[str, Any]:
-    """
-    Transform HTTP response data.
-    
-    Args:
-        response: Response dictionary
-        transformer: Transformation function
-        
-    Returns:
-        Transformed response
-    """
+    """Transform HTTP response data."""
     from gateway import create_error_response
     
     if not response.get('success'):
@@ -181,5 +158,3 @@ __all__ = [
     'create_transformer',
     'create_pipeline',
 ]
-
-# EOF
